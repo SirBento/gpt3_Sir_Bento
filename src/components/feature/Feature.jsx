c0700a43e4dfad008ada68d34d5a051c2cf3af80
Bernard Towindo <bernardtowindo38@gmail.com>
2023-07-13 22:18:50 +0200

    Add files via upload

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './feature.css';
+
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <h1>Features</h1>
+  </div>
+);
+
+export default Feature;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../logo.svg';
+import './navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+             <img src={logo} alt="logo" />
+         </div>
+
+         <div className="gpt3__navbar-links_container" >
+          <p><a href ="#home">Home</a></p>
+          <p><a href ="#wgpt3">What is GPT3?</a></p>
+          <p><a href ="#possibility">Open AI</a></p>
+          <p><a href ="#features">Case Studies</a></p>
+          <p><a href ="#blog">Library</a></p>
+         </div>
+      </div>
+
+    <div className = "gpt3__navbar-sign"></div>
+
+    </div>
+      
+  )};
+
+export default Navbar;
